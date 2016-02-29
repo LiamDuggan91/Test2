e3c4d05ff186c0de58929d7fac5140810068600f
LiamDuggan91 <liamduggan1991@gmail.com>
2016-02-29 11:35:54 +0000

    Initial Commit

--- LiamsFirstProject/src/FirstClass.java
+++ LiamsFirstProject/src/FirstClass.java
@@ -4,5 +4,10 @@ public class FirstClass {
 		System.out.println("Cool");
 		System.out.println("Shortcut");
 		System.out.println("Hello Worlds");
+		
+		int test1 = 25;
+		int test2 = 500232;
+		int modulo = test2%test1;
+		System.out.println(modulo);
 	}
 }
